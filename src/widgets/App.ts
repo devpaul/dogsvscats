5d2b606d6f68399dd647ba8cd5a46e2731c096db
devpaul <paul@devpaul.com>
2018-10-26 21:32:10 -0700

    cat & dog buttons

--- src/widgets/App.ts
+++ src/widgets/App.ts
@@ -1,7 +1,8 @@
-import WidgetBase from '@dojo/framework/widget-core/WidgetBase';
-import { v, w } from '@dojo/framework/widget-core/d';
 import Outlet from '@dojo/framework/routing/Outlet';
-import { Link } from '@dojo/framework/routing/Link';
+import { v, w } from '@dojo/framework/widget-core/d';
+import WidgetBase from '@dojo/framework/widget-core/WidgetBase';
+
+import { Button } from './Button';
 import { CatContainer } from './CatContainer';
 import { DogContainer } from './DogContainer';
 import * as css from './styles/App.m.css';
@@ -9,10 +10,10 @@ import * as css from './styles/App.m.css';
 export default class App extends WidgetBase {
 	protected render() {
 		return v('div', { classes: [css.root] }, [
-			v('header', {}, [
-				w(Link, { to: 'cat' }, [ 'Cats' ]),
+			v('header', { classes: [css.header]}, [
+				w(Button, { to: 'cat' }, [ 'Cats' ]),
 				v('p', {}, [ 'vs' ]),
-				w(Link, { to: 'dog' }, [ 'Dogs' ])
+				w(Button, { to: 'dog' }, [ 'Dogs' ])
 			]),
 			w(Outlet, { key: 'cat', id: 'cat', renderer: () => w(CatContainer, {}) }),
 			w(Outlet, { key: 'dog', id: 'dog', renderer: () => w(DogContainer, {}) })
