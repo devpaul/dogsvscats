43363bcd93becd665904f982ddc392ef54eca20b
devpaul <paul@devpaul.com>
2018-11-09 22:11:24 -0700

    CoreAudio cleanup & moving the sound button to the bottom

--- client/src/widgets/CoreAudio.ts
+++ client/src/widgets/CoreAudio.ts
@@ -5,7 +5,7 @@ export class CoreAudio {
 
 	private audioMap = new Map<string, AudioBuffer>();
 
-	async play(sound: string, speed: number, onStop: () => void) {
+	async play(sound: string, speed: number) {
 		if (!this.context) {
 			this.context = new AudioContext();
 		}
@@ -15,19 +15,16 @@ export class CoreAudio {
 			this.context.resume();
 		}
 
+		const buffer = await this.loadCached(sound);
 		const source = this.context.createBufferSource();
-		source.buffer = await this.loadCached(sound);
+		source.buffer = buffer;
 		source.connect(this.context.destination);
 		source.playbackRate.value = speed;
-		source.loop = true;
 		source.start(this.context.currentTime);
 
-		setTimeout(() => {
-			source.stop();
-			onStop();
-		}, 5000);
-
-		console.log(sound);
+		await new Promise((resolve) => {
+			setTimeout(resolve, buffer.duration);
+		});
 	}
 
 	private async loadCached(sound: string) {
