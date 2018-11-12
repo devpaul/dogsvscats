3419297d6f4efebff94f865ea001c9ae77251ad8
Tom Dye <github@tomdye.co.uk>
2018-11-12 21:29:38 +0000

    add icon and change colors

--- client/src/widgets/styles/app.m.css.d.ts
+++ client/src/widgets/styles/app.m.css.d.ts
@@ -3,4 +3,8 @@ export const button: string;
 export const header: string;
 export const controls: string;
 export const slider: string;
+export const sliderThumb: string;
+export const sliderFill: string;
+export const sliderTrack: string;
 export const buttonContainer: string;
+export const iconSound: string;
