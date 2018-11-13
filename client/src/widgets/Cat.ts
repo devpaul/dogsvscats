b4f12a6b852c3b0d00f2107532f0040c1dd4105b
Tom Dye <github@tomdye.co.uk>
2018-11-13 10:56:04 +0000

    mobile layout

--- client/src/widgets/Cat.ts
+++ client/src/widgets/Cat.ts
@@ -16,7 +16,11 @@ export class Cat extends ThemedMixin(WidgetBase)<CatProperties> {
 	private _getHeadAnimation(animationSpeed: number) {
 		return {
 			id: 'cat-head',
-			effects: [{ marginBottom: '0px' }, { marginBottom: '5px' }, { marginBottom: '0px' }] as any,
+			effects: [
+				{ marginBottom: '0px', transform: 'rotate(0deg)' },
+				{ marginBottom: '7px', transform: 'rotate(-10deg)' },
+				{ marginBottom: '0px', transform: 'rotate(0deg)' }
+			] as any,
 			timing: {
 				duration: 800,
 				iterations: Infinity
@@ -31,7 +35,11 @@ export class Cat extends ThemedMixin(WidgetBase)<CatProperties> {
 	private _getTailAnimation(animationSpeed: number) {
 		return {
 			id: 'cat-tail',
-			effects: [{ transform: 'rotate(0deg)' }, { transform: 'rotate(-10deg)' }, { transform: 'rotate(0deg)' }],
+			effects: [
+				{ transform: 'rotate(0deg)' },
+				{ transform: 'rotate(-10deg)' },
+				{ transform: 'rotate(0deg)' }
+			],
 			timing: {
 				duration: 1000,
 				iterations: Infinity
