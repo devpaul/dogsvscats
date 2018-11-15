69a8edcdd97dda51149e99790bcd1064ab03ee71
Tom Dye <github@tomdye.co.uk>
2018-11-15 16:05:46 +0000

    dist

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,7 @@ const PORT = 3000;
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors();
-	app.useStaticAssets(join(__dirname, '../..', 'client', 'output', 'dev'));
+	app.useStaticAssets(join(__dirname, '../..', 'client', 'output', 'dist'));
 
 	await app.listen(PORT);
 	console.log(`server started on port ${PORT}`);
