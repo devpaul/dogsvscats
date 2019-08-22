6b3558353d3efbd3f4427e3f6cd72d8ff10a894c
devpaul <paul@devpaul.com>
2019-08-22 10:29:05 -0700

    persist the uuid

--- client/src/processes/initialization.ts
+++ client/src/processes/initialization.ts
@@ -1,23 +1,42 @@
+import global from '@dojo/framework/shim/global';
 import { uuid } from '@dojo/framework/core/util';
 import { createCommandFactory, createProcess } from '@dojo/framework/stores/process';
 import { add } from '@dojo/framework/stores/state/operations';
 import has from '@dojo/framework/core/has';
 
-import { State } from '../interfaces';
+import { State, User } from '../interfaces';
 
 const commandFactory = createCommandFactory<State>();
 
+function isUser(value: any): value is User {
+	return value && typeof value === 'object' && typeof value.uuid === 'string';
+}
+
+function fetchUserData(): User {
+	try {
+		const data = JSON.parse(global.localStorage.getItem('user'));
+		if (isUser(data)) {
+			return data;
+		}
+	}
+	catch (e) {}
+
+	const user = { uuid: uuid() };
+	global.localStorage.setItem('user', JSON.stringify(user));
+	return user;
+}
+
 // Command that creates the basic initial state
 export const initialStateCommand = commandFactory(({ path }) => {
+	const user = fetchUserData();
+
 	return [
 		add(path('character'), {
 			choice: undefined,
 			excitement: 1
 		}),
 		add(path('results'), {}),
-		add(path('user'), {
-			uuid: uuid()
-		}),
+		add(path('user'), user),
 		add(path('config'), {
 			title: has('spock-vs-yoda') ? 'Spock vs Yoda' : 'Cats vs Dogs',
 			prompt: has('spock-vs-yoda') ? 'Choose your side: Starfleet or Rebels' : 'Choose your side, Cats or Dogs',
